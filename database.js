418ea8740ac1ff7e78cf8b9c3ccf0c10a4e261d2
aearl23 <64229908+aearl23@users.noreply.github.com>
2024-03-22 13:25:40 -0600

    updates

--- database.js
+++ database.js
@@ -5,9 +5,8 @@ const config = require('./dbConfig.json');
 
 const url = `mongodb+srv://${config.userName}:${config.password}@${config.hostname}`;
 const client = new MongoClient(url);
-const db = client.db('simon');
+const db = client.db('CS260');
 const userCollection = db.collection('user');
-const scoreCollection = db.collection('score');
 
 // This will asynchronously test the connection and exit the process if it fails
 (async function testConnection() {
@@ -40,24 +39,3 @@ async function createUser(email, password) {
   return user;
 }
 
-function addScore(score) {
-  scoreCollection.insertOne(score);
-}
-
-function getHighScores() {
-  const query = { score: { $gt: 0, $lt: 900 } };
-  const options = {
-    sort: { score: -1 },
-    limit: 10,
-  };
-  const cursor = scoreCollection.find(query, options);
-  return cursor.toArray();
-}
-
-module.exports = {
-  getUser,
-  getUserByToken,
-  createUser,
-  addScore,
-  getHighScores,
-};
